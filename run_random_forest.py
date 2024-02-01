2402ae6fcbe1b842664216c6739afd5bb7bf58bf
Chungsang Tom Lam <erinata@gmail.com>
2024-02-01 11:11:24 -0500

    add random forest

--- run_random_forest.py
+++ run_random_forest.py
@@ -2,13 +2,14 @@ import pandas
 import kfold_template
 
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 dataset = pandas.read_csv("temperature_data.csv")
 dataset = pandas.get_dummies(dataset)
 dataset = dataset.sample(frac=1).reset_index()
 
 target = dataset["actual"].values
-data = dataset.drop(["actual","level_0"], axis = 1)
+data = dataset.drop(["actual","level_0","temp_lag1"], axis = 1)
 
 feature_list = data.columns
 data = data.values
@@ -17,12 +18,12 @@ print(feature_list)
 print(target)
 print(data)
 
-machine = tree.DecisionTreeClassifier(criterion="gini", max_depth=5)
+machine = RandomForestClassifier(criterion="gini", max_depth=2, n_estimators=100, bootstrap = True) 
 return_values = kfold_template.run_kfold(machine, data, target, 4, True)
 print(return_values)
 
 
-machine = tree.DecisionTreeClassifier(criterion="gini", max_depth=5)
+machine = RandomForestClassifier(criterion="gini", max_depth=2, n_estimators=100, bootstrap = True) 
 machine.fit(data, target)
 feature_importances_raw = machine.feature_importances_
 print(feature_importances_raw)
